10c2dbb60f5f28ce348939f303711dfa8f79d260
AreHumphrey <lutdina04@yandex.ru>
2024-08-20 22:31:24 +1000

    add appointment_form.html

--- app.py
+++ app.py
@@ -16,12 +16,16 @@ def book():
 @app.route('/doctor/<int:doctor_id>')
 def doctor_detail(doctor_id):
     return render_template('./pages/doctor_detail.html', doctor_id=doctor_id)
+
+
+@app.route('/appointment_form/<int:doctor_id>')
+@app.route('/appointment/<int:doctor_id>')
+def appointment_form(doctor_id):
+    return render_template('./pages/appointment_form.html', doctor_id=doctor_id)
 @app.route('/online')
 def online():
-    # Логика для страницы врачей онлайн
     return "<h1>Врачи онлайн</h1>"
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
